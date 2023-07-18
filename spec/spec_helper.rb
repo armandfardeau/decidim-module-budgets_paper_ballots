bf48ff6285cacfcfdc7d87161eb3a636f66c4f4a
armandfardeau <fardeauarmand@gmail.com>
2023-07-18 15:01:21 +0200

    Fix tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "decidim/dev"
+Decidim::Dev.dummy_app_path = File.expand_path(Rails.root.to_s)
+require "decidim/dev/test/base_spec_helper"
+
+Dir.glob("./spec/support/**/*.rb").sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.formatter = ENV.fetch("RSPEC_FORMAT", "progress").to_sym
+  config.include EnvironmentVariablesHelper
+
+  config.before do
+    # Initializers configs
+    SocialShareButton.configure do |social_share_button|
+      social_share_button.allow_sites = %w(twitter facebook whatsapp_app whatsapp_web telegram)
+    end
+  end
+end
